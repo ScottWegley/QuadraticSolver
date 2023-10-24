c516aecca187bdd11a62b563f8d0c62b74fb32f8
Scott Wegley <scbrwegley@protonmail.ch>
2023-10-24 10:01:06 -0700

    made primitive inputprocesser class live validity tracking, input collection, getter functions

--- src/InputProcessor.java
+++ src/InputProcessor.java
@@ -1,3 +1,34 @@
+import java.util.Scanner;
+
 public class InputProcessor {
-    
+    private double[] inputs;
+    private boolean valid = true;
+
+    public InputProcessor() {
+
+    }
+
+    public void collectInputs() {
+        System.out.print("\nPlease enter three numbers: ");
+        Scanner scanner = new Scanner(System.in);
+        inputs = new double[3];
+        try {
+            for (int i = 0; i < inputs.length; i++) {
+                inputs[i] = scanner.nextDouble();
+            }
+        } catch (Exception e) {
+            valid = false;
+        }
+    }
+
+    public boolean isValid() {
+        return valid;
+    }
+
+    public double[] getInputs() {
+        if(inputs == null){
+            collectInputs();
+        }
+        return inputs;
+    }
 }
