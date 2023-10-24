a9eff6e3071464cbd1069ed26acb7a2164b522c5
Scott Wegley <scbrwegley@protonmail.ch>
2023-10-24 11:28:56 -0700

    basics of main app with input processor

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,5 +1,10 @@
 public class MainApp {
     public static void main(String[] args) {
-        
+        InputProcessor ip = new InputProcessor();
+        if(!ip.isValid()){
+            
+        } else {
+
+        }
     }
 }
